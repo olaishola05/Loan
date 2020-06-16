7e3bd3fe4dce84680a27cf4aabb33db939eff116
Ola Ishola <olaishola@hotmail.co.uk>
2020-06-17 00:35:49 +0100

    worked on the UI

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+const btnApply = document.getElementById("btn-apply");
 
+function openForm() {
+    document.getElementById("loanForm").innerHTML = "Player nine";
+}
+
+btnApply.addEventListener("click", openForm);
