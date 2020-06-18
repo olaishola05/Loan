293a35d23ef593bbe7579367bfc52047e9da6e32
Ola Ishola <olaishola@hotmail.co.uk>
2020-06-18 23:10:27 +0100

    worked on the loan form and a sprinkle of js

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const btnApply = document.getElementById("btn-apply");
+const benchmark = 500000;
 
-function openForm() {
-    document.getElementById("loanForm").innerHTML = "Player nine";
+const amount = document.getElementById("loanAmnt").value;
+const salary = document.getElementById("mntIncome").value;
+
+let interest = 0;
+
+function checkLoan() {
+    // if (amount > benchmark) {
+    //     interest = 8.7;
+    //     alert(interest, amount);
+    // } else {
+    //     interest = 5.7;
+    //     alert(interest, amount);
+    // }
+
+    alert(salary);
 }
 
-btnApply.addEventListener("click", openForm);
+document.getElementById("formbtn").addEventListener("click", checkLoan);
