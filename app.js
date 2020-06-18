808e0af9cffb2acc9038f59c67409ef2cffddf5b
Ola Ishola <olaishola@hotmail.co.uk>
2020-06-19 00:49:19 +0100

    worked on the js and little bit styling

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const benchmark = 500000;
 
-const amount = document.getElementById("loanAmnt").value;
-const salary = document.getElementById("mntIncome").value;
-
 let interest = 0;
+let emi;
+let duration;
 
 function checkLoan() {
-    // if (amount > benchmark) {
-    //     interest = 8.7;
-    //     alert(interest, amount);
-    // } else {
-    //     interest = 5.7;
-    //     alert(interest, amount);
-    // }
+    const amount = document.getElementById("loanAmnt").value;
+    const salary = document.getElementById("mntIncome").value;
 
-    alert(salary);
+    if (amount > benchmark) {
+        interest = 8.7 / 100;
+        duration = 8 * 12;
+        console.log(interest, amount);
+        emi = (amount * interest * (1 + interest / 12) * duration) / (12 * (1 + interest / 12) * duration - 1);
+        console.log(emi);
+    } else {
+        interest = 5.7 / 100;
+        duration = 5 * 12;
+        console.log(interest, amount);
+        emi = (amount * interest * (1 + interest / 12) * duration) / (12 * (1 + interest / 12) * duration - 1);
+        console.log(emi);
+    }
+
+    if (emi >= salary) {
+        console.log("sorry you are not qualified today, you can check back in few weeks");
+    } else {
+        alert("congratulation, we will get back to u asap");
+    }
 }
 
 document.getElementById("formbtn").addEventListener("click", checkLoan);
