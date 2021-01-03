ad389aa760bde1021e802c09ef216c1fa4f99f07
Ola Ishola <olaishola@hotmail.co.uk>
2021-01-03 15:17:01 +0100

    validation working and paged linked

--- app.js
+++ app.js
@@ -5,39 +5,11 @@ let interest = 0;
 let emi;
 let duration;
 
-function validation() {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const day = document.getElementById("day").value;
-    const year = document.getElementById("month").value;
-    const month = document.getElementById("year").value;
-    const purpose = document.getElementById("purpose").value;
-    const working = document.getElementById("exampleCheck1");
-    const notWorking = document.getElementById("exampleCheck2");
-    const loanAmnt = document.getElementById("loanAmnt").value;
-    const income = document.getElementById("mntIncome").value;
-
-    const btn = document.getElementById("formbtn");
-
-    if (
-        name &&
-        email &&
-        day &&
-        month &&
-        year &&
-        purpose &&
-        loanAmnt &&
-        income == ""
-    ) {
-        btn.style.display = "hidden";
-    } else {
-        btn.style.display = "visible";
-    }
-}
-
 function checkLoan(e) {
     e.preventDefault();
-    const amount = document.getElementById("loanAmnt").value;
+    const amount = Number(
+        document.getElementById("loanAmnt").value
+    );
     const salary = Number(
         document.getElementById("mntIncome").value
     );
@@ -50,7 +22,8 @@ function checkLoan(e) {
                     "<h6>The amount you entered is above our maximum</h6>",
                 text: "Pls try again with new Amount",
             },
-            clearFields()
+            clearFields(),
+            validation()
         );
     } else if (amount > minLoan) {
         interest = 8.7 / 100;
@@ -94,7 +67,8 @@ function checkLoan(e) {
                 imageWidth: 400,
                 imageHeight: 200,
             },
-            clearFields()
+            clearFields(),
+            validation()
         );
     } else if (emi > salary) {
         Swal.fire(
@@ -104,7 +78,8 @@ function checkLoan(e) {
                     "<h6>Sorry you are unable to apply today...</h6>",
                 text: "Try again in few months",
             },
-            clearFields()
+            clearFields(),
+            validation()
         );
     }
 }
@@ -120,13 +95,81 @@ function clearFields() {
     document.getElementById("loanAmnt").value = "";
     document.getElementById("mntIncome").value = "";
 }
-document
-    .getElementById("formbtn")
-    .addEventListener("click", checkLoan, clearFields);
+
+// Validation
+const loaded = (window.onload = function () {
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const day = document.getElementById("day");
+    const year = document.getElementById("month");
+    const month = document.getElementById("year");
+    const purpose = document.getElementById("purpose");
+    const working = document.getElementById("exampleCheck1");
+    const notWorking = document.getElementById("exampleCheck2");
+    const loanAmnt = document.getElementById("loanAmnt");
+    const income = document.getElementById("mntIncome");
+
+    const btn = (document.getElementById(
+        "formbtn"
+    ).disabled = true);
+
+    name.onkeyup = validation;
+    email.onkeyup = validation;
+    day.onclick = validation;
+    month.onclick = validation;
+    year.onclick = validation;
+    working.onclick = validation;
+    notWorking.onclick = validation;
+    purpose.onkeyup = validation;
+    loanAmnt.onkeyup = validation;
+    income.onkeyup = validation;
+});
+
+function validation() {
+    console.log("im validated");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const day = document.getElementById("day").value;
+    const year = document.getElementById("month").value;
+    const month = document.getElementById("year").value;
+    const purpose = document.getElementById("purpose").value;
+    const working = document.forms.emp[0];
+    const notWorking = document.forms.emp[1];
+    const loanAmnt = document.getElementById("loanAmnt").value;
+    const income = document.getElementById("mntIncome").value;
+
+    const btn = document.getElementById("formbtn");
+
+    if (
+        name &&
+        email &&
+        day &&
+        month &&
+        year &&
+        purpose &&
+        loanAmnt &&
+        income === ""
+    ) {
+        btn.disabled = true;
+    }
+    if (!working.checked && !notWorking.checked) {
+        btn.disabled = true;
+    } else {
+        btn.disabled = false;
+    }
+
+    if (working.checked) {
+        notWorking.disabled = true;
+    } else {
+        notWorking.disabled = false;
+    }
+    if (notWorking.checked) {
+        working.disabled = true;
+    } else {
+        working.disabled = false;
+    }
+}
 
 document
-    .getElementById("forms")
-    .addEventListener("load", validation);
-// document
-//     .getElementById("formBtn")
-//     .addEventListener("click", clearFields);
+    .getElementById("formbtn")
+    .addEventListener("click", checkLoan);
